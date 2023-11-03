a319f436c806842cae1d997e4c54ca46868ea3e5
Pagebakers <eelco@appulse.nl>
2023-11-03 17:54:31 +0100

    feat: add command

---
+++ packages/cli/src/commands/foo.ts
@@ -0,0 +1,30 @@
+import {Args, Command, Flags} from '@oclif/core'
+
+export default class Foo extends Command {
+  static description = 'describe the command here'
+
+  static examples = [
+    '<%= config.bin %> <%= command.id %>',
+  ]
+
+  static flags = {
+    // flag with a value (-n, --name=VALUE)
+    name: Flags.string({char: 'n', description: 'name to print'}),
+    // flag with no value (-f, --force)
+    force: Flags.boolean({char: 'f'}),
+  }
+
+  static args = {
+    file: Args.string({description: 'file to read'}),
+  }
+
+  public async run(): Promise<void> {
+    const {args, flags} = await this.parse(Foo)
+
+    const name = flags.name ?? 'world'
+    this.log(`hello ${name} from /Users/eelco/Development/appulse/boilerkit/packages/cli/src/commands/foo.ts`)
+    if (args.file && flags.force) {
+      this.log(`you input --force and --file: ${args.file}`)
+    }
+  }
+}
