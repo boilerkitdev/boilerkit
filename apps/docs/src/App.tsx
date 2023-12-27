2c9b7207a2dfaac26bce571b54097ff5261a19c3
Roman Filippov <rf@romanfilippov.com>
2023-12-27 21:22:29 +0000

    fix: disable filename case

--- apps/docs/src/App.tsx
+++ apps/docs/src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable unicorn/filename-case */
 export function App() {
   return <></>
 }
--- apps/docs/src/index.tsx
+++ apps/docs/src/index.tsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client'
 
 import { App } from './App'
 
-const rootElement = document.getElementById('root')
+const rootElement = document.querySelector('#root')
 
 if (!rootElement) {
   throw new Error('Cannot find #root element.')
